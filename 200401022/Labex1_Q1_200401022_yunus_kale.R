60ec8848fa6319ee59e9da66dde9d89dfc7e8459
Yunus Kale <yunus_kale_@hotmail.com>
2023-11-17 01:42:25 +0300

    1.2, testleri ile tamamlandı

--- 200401022/Labex1_Q1_200401022_yunus_kale.R
+++ 200401022/Labex1_Q1_200401022_yunus_kale.R
@@ -2,3 +2,7 @@
 url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
 download.file(url, destfile = "MapsThatChangedOurWorld_StoryMap_Data.csv")
 
+# 1.2
+maps <- read.csv("MapsThatChangedOurWorld_StoryMap_Data.csv",header = TRUE, sep = ";")
+maps = as.data.frame(maps)
+
--- 200401022/Labex1_Q1_tests_200401022_yunus_kale.R
+++ 200401022/Labex1_Q1_tests_200401022_yunus_kale.R
@@ -9,8 +9,34 @@ if (file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv")) {
 source("Labex1_Q1_200401022_yunus_kale.R")
 library(testthat)
 
+# 2.4
 test_that("MapsThatChangedOurWorld_StoryMap_Data.csv adlı dosya aktif dizinde mevcuttur", {
   file_path <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
   testthat::expect_true(file.exists(file_path), 
                         "Dosya bulunamadı. Lütfen dosyanın aktif dizinde olduğundan emin olun.")
+})
+
+# 2.5
+test_that("maps adlı değişken Global Workspace'te mevcuttur", {
+  testthat::expect_true(exists("maps", envir = .GlobalEnv), 
+                        "maps adlı değişken Global Workspace'te bulunamadı.")
+})
+
+# 2.6
+test_that("maps nesnesi bir data.frame'dir", {
+  testthat::expect_true(is.data.frame(maps), 
+                        "maps nesnesi bir data.frame değildir.")
+})
+
+# 2.7
+test_that("maps adlı data.frame'in ilk sütunun adı 'City' olmalıdır", {
+  testthat::expect_identical(names(maps)[1], "City", 
+                             info = "maps data.frame'in ilk sütununun adı 'City' olmalıdır.")
+})
+
+# 2.8
+test_that("maps adlı data.frame'in 5. sütunun adında 'Title' kelimesi geçmelidir", {
+  expected_keyword <- "Title"
+  testthat::expect_true(any(grepl(expected_keyword, names(maps)[5])), 
+                        paste("maps data.frame'in sütun adları arasında '", expected_keyword, "' kelimesi geçmemektedir."))
 })
